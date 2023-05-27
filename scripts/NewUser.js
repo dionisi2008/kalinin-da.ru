115827bf753022176c8e7186d9eb04bbcde3de0e
Kalinin Denis <dionisi2008@gmail.com>
2023-05-27 17:13:30 +0700

    321

---
+++ scripts/NewUser.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => 
+{
+    document.getElementById("Button").onclick = () =>
+    {
+        alert("test");
+    };
+});
---
+++ scripts/api.js
@@ -0,0 +1,11 @@
+var ClientServer = new XMLHttpRequest();
+
+function NewUser(Login, Password, Email)
+{
+    ClientServer.open("POST", "/api")
+    ClientServer.send("Пользователь" + "\r\n" + "Регистрация пользователя" + "\r\n" + Login + " " + Password + " " + Email);
+    ClientServer.onloadend = () =>
+    {
+        return ClientServer.responseText;
+    };
+}
