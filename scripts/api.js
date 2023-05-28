143cdc41511890db347abaa49beabc3349c919ec
Kalinin Denis <dionisi2008@gmail.com>
2023-05-28 12:09:02 +0700

    123

--- scripts/api.js
+++ scripts/api.js
@@ -1,11 +1,11 @@
 var ClientServer = new XMLHttpRequest();
+xhr.responseType = "text"; // устанавливаем тип ответа как текст
+xhr.overrideMimeType("text/plain; charset=utf-8"); // явно указываем кодировку
 
-function NewUser(Login, Password, Email)
-{
+function NewUser(Login, Password, Email) {
     ClientServer.open("POST", "/api")
     ClientServer.send("Пользователь" + "\r\n" + "Регистрация пользователя" + "\r\n" + Login + " " + Password + " " + Email);
-    ClientServer.onloadend = () =>
-    {
+    ClientServer.onloadend = () => {
         return ClientServer.response;
     };
 }
